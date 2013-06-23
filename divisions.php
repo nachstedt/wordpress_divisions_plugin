756f849026d88b8fc8d225b78c8afc7f126b240d
Timo Nachstedt <timo.nachstedt@googlemail.com>
2013-06-23 18:55:23 +0200

    now using customn post type

--- divisions.php
+++ divisions.php
@@ -12,7 +12,7 @@ License: GPL2
 /*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : PLUGIN AUTHOR EMAIL)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -37,8 +37,7 @@ if(!class_exists('TN_Divisions_Plugin'))
 			// register actions
 			add_action('admin_init', array(&$this, 'admin_init'));
 			add_action('admin_menu', array(&$this, 'add_menu'));
-			add_action( 'plugins_loaded', array(TN_Divisions_Plugin, 'install') );
-				
+			add_action('init', array(&$this, 'create_post_type'));
 		}
 
 		/**
@@ -53,17 +52,29 @@ if(!class_exists('TN_Divisions_Plugin'))
 				'tn_divisions_plugin',                # menu slug
 				array(&$this, 'plugin_settings_page') # callback
 			);
-			
+
 			add_submenu_page(
-				'tn_divisions_plugin',                # parent_slug 
+				'tn_divisions_plugin',                # parent_slug
 				'Division Plugin Settings',           # page_title
-				'Division Plugin',                    # menu_title 
-				'manage_options',                     # required capability 
+				'Division Plugin',                    # menu_title
+				'manage_options',                     # required capability
 				'tn_divisions_plugin',                # menu_slug (same as parent)
 				array(&$this, 'plugin_settings_page') # callback
 			);
+
+			/*
+			add_submenu_page(
+				'tn_divisions_plugin',            # parent_slug
+				'Manage your Divisions',          # page_title
+				'Divisions',                      # menu_title
+				'manage_options',                 # required capability
+				'tn_divisions_manage',            # menu_slug (same as parent)
+			array(&$this, 'manage_divisions')     # callback
+			);
+			*/
+
 		}
-		
+
 		/**
 		 * hook into WP's admin_init action hook
 		 */
@@ -74,6 +85,38 @@ if(!class_exists('TN_Divisions_Plugin'))
 			// Possibly do additional admin_init tasks
 		}
 
+		public function create_post_type()
+		{
+			register_post_type(
+				'dvs_divisions',
+				array(
+					'labels'              => array(
+						'name' => __( 'Divisions' ),
+						'singular_name' => __( 'Division' )
+					),
+					'public'               => true,
+					'exclude_from_search'  => true,
+					'publicly_queryable'   => false,
+					'show_ui'              => true,
+					'show_in_nav_menus'    => false,
+					'show_in_menu'         => true,
+					'has_archive'          => false,
+					'supports'             => array(
+						'title',
+					),
+					'rewrite'              => false,
+					'register_meta_box_cb' => array(&$this, 'meta_box_callback')
+			)
+			);
+		}
+
+		public function meta_box_callback()
+		{
+			echo '<style>#edit-slug-box{display:none;}</style>';
+			#remove_meta_box('submitdiv', 'dvs_divisions', 'side');
+			remove_meta_box('slugdiv', 'dvs_divisions', 'side');
+		}
+
 		/**
 		 * Initialize some custom settings
 		 */
@@ -82,9 +125,9 @@ if(!class_exists('TN_Divisions_Plugin'))
 			// register the settings for this plugin
 			register_setting('tn_divisions_plugin-settings', 'setting_a');
 			register_setting('tn_divisions_plugin-settings', 'setting_b');
-			add_settings_section( 
+			add_settings_section(
 				'section-one',                         # id
-				'Section One',                         # title 
+				'Section One',                         # title
 				array(&$this, 'section_one_callback'), # callback
 				'tn_divisions_plugin'                  # menu slug
 			);
@@ -104,8 +147,8 @@ if(!class_exists('TN_Divisions_Plugin'))
 				'section-one',                       # section id
 				array('name' => 'setting_b')         # callback args
 			);
-		} 
-		
+		}
+
 		/**
 		 * Menu Callback
 		 */
@@ -117,30 +160,30 @@ if(!class_exists('TN_Divisions_Plugin'))
 					'You do not have sufficient permissions to access this '
 					.'page.'));
 			}
-		
+
 			// Render the settings template
 			include(sprintf("%s/templates/settings.php", dirname(__FILE__)));
-		}		
-		
+		}
+
 		public function section_one_callback()
 		{
 			echo 'Some help text goes here.';
 		}
-		
+
 		function setting_callback( $args ) {
 			$name = esc_attr( $args['name'] );
 			$value = esc_attr( get_option( $name ) );
 			echo "<input type='text' name=$name value='$value' />";
 		}
-		
+
 		/**
 		 * Activate the plugin
 		 */
 		public static function activate()
 		{
-			TN_Divisions_Plugin::install();
-		}		
-		
+			// do nothing
+		}
+
 		/**
 		 * Deactivate the plugin
 		 */
@@ -148,28 +191,7 @@ if(!class_exists('TN_Divisions_Plugin'))
 		{
 			// Do nothing
 		}
-		
-		public static function install() {
-			global $wpdb;
-		
-			$installed_ver = get_option('tn_divisions_db_version', "0.0.0");
-		
-			if( $installed_ver != "0.0.1") {
-				$table_name = $wpdb->prefix . 'tn_divisions';
-		
-				$sql = "CREATE TABLE $table_name (
-				id mediumint(9) NOT NULL AUTO_INCREMENT,
-				name tinytext NOT NULL,
-				UNIQUE KEY id (id)
-				);";
-					
-				require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-				dbDelta( $sql );
-		
-						add_option( "tn_divisions_db_version", "0.0.1" );
-			}
-		}
-		
+
 	}
 }
 
@@ -185,19 +207,22 @@ if(class_exists('TN_Divisions_Plugin'))
 	// instantiate the plugin class
 	$tn_divisions_plugin = new TN_Divisions_Plugin();
 	if(isset($tn_divisions_plugin)) {
-		// Add the settings link to the plugins page 
-		function plugin_settings_link($links) { 
-			$settings_link = 
-				'<a href="' 
-				. get_bloginfo('wpurl') 
-				. '/wp-admin/admin.php?page=tn_divisions_plugin">Settings</a>'; 
-			array_unshift($links, $settings_link); 
-			return $links; 
-		} 
-		$plugin = plugin_basename(__FILE__); 
-		add_filter("plugin_action_links_$plugin", 'plugin_settings_link'); 
+		// Add the settings link to the plugins page
+		function plugin_settings_link($links) {
+			$settings_link =
+				'<a href="'
+				. get_bloginfo('wpurl')
+				. '/wp-admin/admin.php?page=tn_divisions_plugin">Settings</a>';
+			array_unshift($links, $settings_link);
+			return $links;
+		}
+		$plugin = plugin_basename(__FILE__);
+		add_filter("plugin_action_links_$plugin", 'plugin_settings_link');
+
+
 	}
 }
+
 
 /*
 function modify_link($permalink_url, $post_data)  {
