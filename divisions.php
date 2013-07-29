bd4859c6709961aeaac4b173bb95db7ea9a641d3
Timo Nachstedt <nachstedt@braincorporation.com>
2013-07-29 22:40:43 +0200

    removed comment

--- divisions.php
+++ divisions.php
@@ -227,8 +227,6 @@ if(!class_exists('TN_Divisions_Plugin'))
 		}
 
 		function edit_nav_menu_walker( $walker ) {
-			//@TODO this should be loaded somewhere sooner...
-
 			// swap the menu walker class only if it's the default wp class (just in case)
 			if ( $walker === 'Walker_Nav_Menu_Edit' ) {
 				require_once WP_PLUGIN_DIR . '/divisions/includes/divisions_walker_nav_menu_edit.php';
